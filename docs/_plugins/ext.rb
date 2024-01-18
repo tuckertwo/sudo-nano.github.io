32698deb5d750cad74ac1f2d1e02f66df5234ea0
Connor <cwg1231@gmail.com>
2024-01-18 00:01:47 -0800

    Fixed gemfile configuration for jekyll-tagging plugin

--- docs/_plugins/ext.rb
+++ docs/_plugins/ext.rb
@@ -1,2 +1,3 @@
 # The github instructions say to use single quotes here, but it's not working so I'm trying double quotes
-require "jekyll/tagging"
+# they also say to put 'jekyll/tagging' but I don't know if that slash is a typo, so I'm trying a dash
+require "jekyll-tagging"
