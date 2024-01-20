44eb8a2b66de3699ebec0e171358dbaea664c17b
Connor <cwg1231@gmail.com>
2024-01-20 00:22:51 -0800

    Modified ext.rb to align with tutorial for jekyll-tagging

--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -1,3 +1,3 @@
 # The github instructions say to use single quotes here, but it's not working so I'm trying double quotes
 # they also say to put 'jekyll/tagging' but I don't know if that slash is a typo, so I'm trying a dash
-require "jekyll-tagging"
+require "jekyll/tagging"
